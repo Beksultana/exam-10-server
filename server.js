3cb5907cfbfbb327987a7bd3b4ef7adccf798ec6
sultan <aidaraliuulu@gmail.com>
2019-03-30 12:31:56 +0600

    В package.json устоновил nodemon, создал файл news.js где делаю get запрос на сервер. А в server.js подключил и нанисал все нужные коды.

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql');
+const news = require('./app/news');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const port = 8800;
+
+const connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'user2',
+    password : '1qaz@WSX29',
+    database : 'news'
+});
+
+app.use('/news', news(connection));
+
+connection.connect(error => {
+    if (error) {
+        console.error('error connecting: ' + error.stack)
+    }
+    console.log('connected as id ' + connection.threadId);
+
+    app.listen(port, () => {
+        console.log(`Server started on ${port} port`)
+    });
+});
